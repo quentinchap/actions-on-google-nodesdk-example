4e789d290c4a30441c71d9a096c4897d5054b291
CHAPELLE Quentin <chapelle.quentin@gmail.com>
2018-05-26 14:52:04 +0200

    update actions

--- index.js
+++ index.js
@@ -8,7 +8,19 @@ const {actionssdk} = require('actions-on-google');
 const app = actionssdk({debug: true});
 
 app.intent('actions.intent.MAIN', (conv) => {
-  conv.ask('Hi!');
+  conv.ask('Salut!');
 });
+
+app.intent('actions.intent.TEXT', handleTextIntent);
+
+function handleTextIntent(conv, input) {
+    if(input === 'Allume la lampe'){
+        conv.ask('Comme si c\'était fait!');
+    }
+    else{
+        conv.ask('Désolé mais je n\'ai pas compris.');
+    }
+  }
+
 
 express().use(bodyParser.json(), app).listen(3000)
