304e77d9cdd30a89d3ef3bb7f9b383da4363cb59
CHAPELLE Quentin <chapelle.quentin@gmail.com>
2018-05-26 15:37:04 +0200

    update actions

--- index.js
+++ index.js
@@ -3,24 +3,27 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
-const {actionssdk} = require('actions-on-google');
+const {actionssdk, Image} = require('actions-on-google');
 
 const app = actionssdk({debug: true});
 
 app.intent('actions.intent.MAIN', (conv) => {
-  conv.ask('Salut!');
+    conv.ask('Salut!');
+    conv.ask(new Image({
+        url: 'https://i2.wp.com/devotics.fr/wp-content/uploads/2017/08/cropped-logo-tranparent' +
+                '-400x434-1.png?fit=400%2C438&ssl=1',
+        alt: 'Devotics'
+    }))
 });
 
 app.intent('actions.intent.TEXT', handleTextIntent);
 
 function handleTextIntent(conv, input) {
-    if(input === 'Allume la lampe'){
+    if (input === 'Allume la lampe') {
         conv.ask('Comme si c\'était fait!');
-    }
-    else{
+    } else {
         conv.ask('Désolé mais je n\'ai pas compris.');
     }
-  }
-
+}
 
 express().use(bodyParser.json(), app).listen(3000)
